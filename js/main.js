3450d69e693d1290ae299f06264c6a6b32b74ffa
Graphic <graphic@SOD-SJH-516-05M.local>
2015-09-25 14:41:00 -0400

    submit button error class added to inputs and remove error class

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$('.signin')
-	.on('click', function(e) { 
+$('.signin').on('click', function(e) { 
 			$('.modal').fadeIn(200, function(e){
 				$('.close').on('click', function(e){
 					$('.modal').fadeOut(200, function(e){})
 				})
+
+			$('.submit').on('click', function(e) {
+				$('input').addClass('error');
+			})
+
+			$('input').on('click', function(e){
+				$(this).removeClass('error');
+			})
+
 			});
 		});
